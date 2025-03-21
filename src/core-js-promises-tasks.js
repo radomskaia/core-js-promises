e7459c5da52c2a8b5287a8b0d0ced931fb1af0cb
Alena Radomskaia <radomskaia.a.i@gmail.com>
2025-03-21 16:45:09 +0700

    feat: add part of solution

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -96,10 +96,9 @@ function getFirstPromiseResult(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+function getAllOrNothing(promises) {
+  return Promise.all(promises).then((responses) => responses);
 }
-
 /**
  * Processes an array of promises and returns a promise that resolves with an array of their results.
  * Each element in the returned array corresponds to the resolved value of the promise at the same index in the input array.
@@ -112,8 +111,10 @@ function getAllOrNothing(/* promises */) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return Promise.allSettled(promises).then((responses) =>
+    responses.map((response) => response.value)
+  );
 }
 
 /**
@@ -134,8 +135,12 @@ function getAllResult(/* promises */) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  return promises.reduce((acc, promise) => {
+    return acc.then((result) =>
+      promise.then((value) => result.toString() + value)
+    );
+  });
 }
 
 module.exports = {
